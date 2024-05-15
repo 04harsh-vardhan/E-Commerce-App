1ef0cc840fc4c8b7268951cc97297956120ca06d
HarshVardhan <143250140+1FPSLYF@users.noreply.github.com>
2024-05-15 10:42:07 +0530

    done-adding-state

--- buyzon/app.vue
+++ buyzon/app.vue
@@ -4,4 +4,10 @@
   </div>
 </template>
 <script setup>
+  const productData = useProductData();
+  await callOnce(async () => {
+    const { data } = await useFetch("https://fakestoreapi.com/products");
+    productData.value = toRaw(data.value);
+  });
+  
 </script>
---
+++ buyzon/composables/states.ts
@@ -0,0 +1,13 @@
+export const useProductData = () => useState<[ProductData]>("productData");
+type ProductData = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
