e6746aa243d6300a42890cac728169f941c5eac8
HarshVardhan <143250140+1FPSLYF@users.noreply.github.com>
2024-05-15 09:08:16 +0530

    minor-changes

--- buyzon/app.vue
+++ buyzon/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 <script setup>
+  // const { data } = await useFetch("https://fakestoreapi.com/products");
+  // console.log("data ", toRaw(data.value));
 </script>
