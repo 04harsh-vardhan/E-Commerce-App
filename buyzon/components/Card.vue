51ad5d1ad2dcc0c6775b597b3c9a450cda3e5568
HarshVardhan <143250140+1FPSLYF@users.noreply.github.com>
2024-06-17 12:24:54 +0530

    item-count-in-cart-view

--- buyzon/components/Card.vue
+++ buyzon/components/Card.vue
@@ -13,7 +13,7 @@
       <p class="product-price">
         Rs <slot name="price"></slot> <slot name="wishIcon" :id="id"></slot>
       </p>
-      <p class="quantity">Quantity <slot name="quantity"></slot></p>
+      <p class="quantity"><slot name="quantity"></slot></p>
       <slot name="button" :id="id"></slot>
       <slot name="msg"></slot>
     </div>
--- buyzon/pages/dashboard/cart.vue
+++ buyzon/pages/dashboard/cart.vue
@@ -22,7 +22,7 @@
         >
           <template #price>{{ price }}</template>
           <template #title>{{ title }}</template>
-          <template #quantity>{{ quantity }}</template>
+          <template #quantity>Quantity {{ quantity }}</template>
         </Card>
       </div>
     </div>
